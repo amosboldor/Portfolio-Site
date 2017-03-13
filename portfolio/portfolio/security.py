fda9d2227792cd66b99d6255ab7de50f290305e8
Amos Boldor <amosboldor@gmail.com>
2017-03-13 11:38:15 -0700

    add login and security file

---
+++ portfolio/portfolio/security.py
@@ -0,0 +1,58 @@
+"""Security configurations."""
+
+import os
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+from pyramid.security import Everyone, Authenticated
+from pyramid.security import Allow
+from passlib.apps import custom_app_context as pwd_context
+from pyramid.session import SignedCookieSessionFactory
+
+
+def check_credentials(username, password):
+    """Check credentials."""
+    stored_username = os.environ.get('AUTH_USERNAME', '')
+    stored_password = os.environ.get('AUTH_PASSWORD', '')
+    is_authenticated = False
+    if stored_username and stored_password:
+        if username == stored_username:
+            try:
+                is_authenticated = pwd_context.verify(password,
+                                                      stored_password)
+            except ValueError:
+                # ValueError is raised if the stored password
+                # is not hashed or if the salt is improper
+                pass
+    return is_authenticated
+
+
+class MyRoot(object):
+    """MyRoot."""
+
+    def __init__(self, request):
+        """Init for MyRoot Class."""
+        self.request = request
+
+    __acl__ = [
+        (Allow, Everyone, 'view'),
+        (Allow, Authenticated, 'edit'),
+        (Allow, Authenticated, 'create'),
+        (Allow, Authenticated, 'add')
+    ]
+
+
+def includeme(config):
+    """Security-related configuration."""
+    auth_secret = os.environ.get('AUTH_SECRET', 'blahblah')
+    authn_policy = AuthTktAuthenticationPolicy(
+        secret=auth_secret,
+        hashalg='sha3'
+    )
+    authz_policy = ACLAuthorizationPolicy()
+    config.set_authentication_policy(authn_policy)
+    config.set_authorization_policy(authz_policy)
+    config.set_root_factory(MyRoot)
+    session_secret = os.environ.get('SESSION_SECRET', 'blahblah')
+    session_factory = SignedCookieSessionFactory(session_secret)
+    config.set_session_factory(session_factory)
+    config.set_default_csrf_options(require_csrf=True)
--- portfolio/portfolio/views/default.py
+++ portfolio/portfolio/views/default.py
@@ -1,9 +1,34 @@
 """Views for profolio app."""
 from pyramid.response import Response
 from pyramid.view import view_config
+from pyramid.security import remember, forget
+from ..security import check_credentials
+from pyramid.httpexceptions import HTTPFound
 
 
 @view_config(route_name='home', renderer='../templates/home.jinja2')
 def home(request):
     """Home Page View."""
     return {}
+
+
+@view_config(route_name='login',
+             renderer='../templates/login.jinja2',
+             require_csrf=False)
+def login(request):
+    """Login View."""
+    if request.method == 'POST':
+        username = request.params.get('Username', '')
+        password = request.params.get('Password', '')
+        if check_credentials(username, password):
+            headers = remember(request, username)
+            return HTTPFound(location=request.route_url('home'),
+                             headers=headers)
+    return {}
+
+
+@view_config(route_name='logout')
+def logout(request):
+    """Logout view."""
+    headers = forget(request)
+    return HTTPFound(request.route_url('home'), headers=headers)
